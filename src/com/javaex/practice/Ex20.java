158a5458491975db441442ecf539cd84de1b504e
jkm8369 <jkm8369@naver.com>
2025-04-29 18:13:57 +0900

    2025-04-29 18:13

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -8,27 +8,74 @@ public class Ex20 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("===================================");
-		System.out.println("\t[숫자 맞추기 게임 시작]");
-		System.out.println("===================================");
-		
-		
-		int num = (int)(Math.random()*100) + 1;
-		boolean run = true;
-		
-		while(run) {
-			System.out.print(">>");
+		//
+		while(true) {
+
+			System.out.println("===================================");
+			System.out.println("\t[숫자 맞추기 게임 시작]");
+			System.out.println("===================================");
+			
+			
+			int num = (int)(Math.random()*100) + 1;
+			int inputNum;
+			String yn;
 			System.out.println(num);
-			if(num>num) {
-				System.out.println("더 낮게");
+			while(true) {
+				System.out.print(">>");
+				inputNum = sc.nextInt();
+				if(inputNum < num ) {
+					System.out.println("더 높게");
+					
+				} else if(inputNum > num) {
+					System.out.println("더 낮게");
+					
+				} else{
+					System.out.println("맞았습니다.");
+					break;
+				}
+					
+			}
+					
+			sc.nextLine();
+			
+			System.out.print("게임을 종료하시겠습니까? (y/n) >>");
+			yn = sc.nextLine();
+			if("y".equals(yn)) {
+				System.out.println("===================================");
+				System.out.println("\t[숫자 맞추기 게임 종료]");
+				System.out.println("===================================");	
+		
+				break;
 			}
 			
+			
+			
+			
 		}
-		
-		
-		
-		
-		sc.close();
-	}
-
+		sc.close();	
+	}//while
+	
 }
+				
+			 
+
+		
+		
+		
+		
+	
+	
+
+		
+		
+				
+				
+				
+				
+		
+		
+		
+		
+		
+		
+			
