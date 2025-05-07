53b7b8e93e813f736ba5cfee69b8a9c7a34dfa49
jkm8369 <jkm8369@naver.com>
2025-05-07 19:37:39 +0900

    2025-05-07 19:36

---
+++ src/com/javaex/review/Ex02.java
@@ -0,0 +1,24 @@
+package com.javaex.review;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		int x,y;
+		for(x=0; x<4; x++) {
+			for(y=0; y<2; y++) {
+				System.out.println("*");
+			}
+			System.out.println("");
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/javaex/review/Ex08.java
@@ -0,0 +1,22 @@
+package com.javaex.review;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		for(int dan = 2; dan<=9; dan++) {
+			for(int num = 1; num<=9; num++) {
+				System.out.println(dan + "*" + num + "=" + dan*num);
+			}
+			
+		}
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
